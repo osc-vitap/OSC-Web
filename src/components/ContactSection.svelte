75f5bd640400f5a03802b6d7821739c54ccc8f63
vulcanx <soumyadip.chowdhuri@gmail.com>
2023-06-07 18:48:11 +0530

    changed button color

--- src/components/ContactSection.svelte
+++ src/components/ContactSection.svelte
@@ -75,9 +75,10 @@
 					<div class="mt-4">
 						<button
 							type="submit"
-							class="inline-block w-full text-sm rounded-lg bg-black px-2 py-2 font-medium text-white sm:w-auto"
+							class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+							
 						>
-							Send Enquiry
+							Send Enquiry?
 						</button>
 					</div>
 				</form>
