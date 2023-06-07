3ec02ccaca569dcc5c0f73810c64ed7d86c1d065
vulcanx <soumyadip.chowdhuri@gmail.com>
2023-06-07 19:14:51 +0530

    modified email and phone number fields

--- src/components/ContactSection.svelte
+++ src/components/ContactSection.svelte
@@ -30,7 +30,7 @@
 					<div>
 						<label class="sr-only" for="name">Name</label>
 						<input
-							class="w-full rounded-lg border-gray-200 p-3 text-sm"
+							class="w-full rounded-lg border-gray-300 p-3 text-sm"
 							placeholder="Name"
 							type="text"
 							id="name"
@@ -39,23 +39,21 @@
 
 					<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
 						<div>
-							<label class="sr-only" for="email">Email</label>
-							<input
-								class="w-full rounded-lg border-gray-200 p-3 text-sm"
-								placeholder="Email address"
-								type="email"
-								id="email"
-							/>
+							<div class="relative mb-6">
+							<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+								<svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+							</div>
+							<input type="text" id="input-group-1" class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@mail.com">
+							</div>
 						</div>
 
 						<div>
-							<label class="sr-only" for="phone">Phone</label>
-							<input
-								class="w-full rounded-lg border-gray-200 p-3 text-sm"
-								placeholder="Phone Number"
-								type="tel"
-								id="phone"
-							/>
+							<div class="relative mb-6">
+								<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#6b7280" d="M19.95 21q-3.225 0-6.287-1.438t-5.425-3.8q-2.363-2.362-3.8-5.425T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.225t.325.575l.65 3.5q.05.35-.013.638T9.4 8.45L7 10.9q1.05 1.8 2.625 3.375T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.075.575.338T21 15.9v4.05q0 .45-.3.75t-.75.3Z"></path></svg>
+								</div>
+								<input type="text" id="input-group-1" class="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Phone Number">
+							  </div>
 						</div>
 					</div>
 
@@ -75,9 +73,7 @@
 					<div class="mt-4">
 						<button
 							type="submit"
-							class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-							
-						>
+							class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
 							Send Enquiry?
 						</button>
 					</div>
