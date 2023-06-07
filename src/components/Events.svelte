1331eb5250c3feeb0ef9fcca019eb94e871f892d
vulcanx <soumyadip.chowdhuri@gmail.com>
2023-06-08 01:15:11 +0530

    minor changes

--- src/components/Events.svelte
+++ src/components/Events.svelte
@@ -44,7 +44,7 @@
         </div>
           <div div class="text- flex items-center text-center justify-center pt-7">
             <div class="mt-5 mb-5 grid grid-cols-2 gap-4 md:gap-2 lg:grid-cols-3">
-                <Card img="https://oscvitap.org/static/media/4.f0c6291e2c00644ded8f.jpg">
+                  <Card img="https://oscvitap.org/static/media/4.f0c6291e2c00644ded8f.jpg">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cyber Syndicate</h5>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
                       OSC organized a thrilling CTF event, challenging participants to navigate through cybersecurity puzzles and capture the flag.
@@ -53,26 +53,26 @@
                       Jan 21, 2023 | 4 PM
                     </Badge>
                   </Card>
-
+                  <Card img="src\images\TechCastep4.jpg">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">TechCast Ep. 4</h5>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+                      TechCast, OSC's online guest lecture series, transported students into the minds of industry experts, enriching their knowledge and inspiring innovation.
+                    </p>
+                    <Badge color="dark">
+                      May 25, 2023 | 4 PM
+                    </Badge>
+                  </Card>
+                  <Card img="src\images\GDSC.jpg">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">SDG Open Hack</h5>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+                      SDG Open Hack united minds and technology, fostering collaborative solutions for a sustainable future.
+                    </p>
+                    <Badge color="dark">
+                      Mar 12, 2023 | 10 AM
+                    </Badge>
+                  </Card>
                   
-                    <Card img="src\images\TechCastep4.jpg">
-                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">TechCast Ep. 4</h5>
-                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-                        TechCast, OSC's online guest lecture series, transported students into the minds of industry experts, enriching their knowledge and inspiring innovation.
-                      </p>
-                      <Badge color="dark">
-                        May 25, 2023 | 4 PM
-                      </Badge>
-                    </Card>
-                    <Card img="src\images\GDSC.jpg">
-                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">SDG Open Hack</h5>
-                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-                        SDG Open Hack united minds and technology, fostering collaborative solutions for a sustainable future.
-                      </p>
-                      <Badge color="dark">
-                        Mar 12, 2023 | 10 AM
-                      </Badge>
-                    </Card>
+                    
             </div>
             
           </div>
