945bcbdcb84c7e7397a733144f50debbeaef5a79
vulcanx <soumyadip.chowdhuri@gmail.com>
2023-06-08 10:33:15 +0530

    some minor changes

---
+++ src/routes/events-2023/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Events from '../../components/Events.svelte';
+</script>
+
+
+<div class="flex flex-col items-center justify-between max-w-full">
+    <Events/>
+  </div>
+
+
+
