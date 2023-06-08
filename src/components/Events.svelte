b1088d8a1c9dc1eec9ba8c3003fc044f1b7c0f3c
vulcanx <soumyadip.chowdhuri@gmail.com>
2023-06-08 11:40:17 +0530

    major changes to structure added all pages 2023 -- 2018

--- src/components/Events.svelte
+++ src/components/Events.svelte
@@ -1,27 +1,15 @@
 <script>
-    import { Card,GradientButton, Badge,Button } from 'flowbite-svelte';
-    let isHovered=false;
+    import { Card,Button, Badge, GradientButton } from 'flowbite-svelte';
 </script>
 
-<style>
-  .card{
-    transition: transform 0.3s;
-  }
 
-  .card:hover{
-    transform:scale(1.2)
-  }
-</style>
-
-
-<section class="bg-transparent py-8">
-	<div class="mx-auto  px-4 py-2 sm:px-6 lg:px-8">
-        <div class="text-5xl font-extrabold text-white py-12 lg:px-4 text-left">
+<section class="bg-transparent ">
+	
+        <div class="text-5xl font-extrabold text-white py-12 lg:px-4 flex justify-left">
             Events
         </div>
         <div class="items-center text-center flex justify-center">
             <ul class="text- flex items-center gap-6 text-sm">
-                
                 <li>
                     <a href="/events-2023">
                       <GradientButton outline color="purpleToBlue" >2023</GradientButton>
@@ -54,50 +42,4 @@
                 </li>                                                                      
             </ul>
         </div>
-          <div >
-            <div class="mt-5 mb-5 grid grid-cols-2 gap-4 md:gap-6 lg:grid-cols-3">
-              <div class="card" on:mouseenter={() => isHovered = true} on:mouseleave={() => isHovered = false}>
-                <Card img="https://oscvitap.org/static/media/4.f0c6291e2c00644ded8f.jpg">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cyber Syndicate</h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight pb-6">
-                      OSC organized a thrilling CTF event, challenging participants to navigate through cybersecurity puzzles and capture the flag.
-                    </p>
-                    <Badge color="dark">
-                      Jan 21, 2023 | 4 PM
-                    </Badge>
-                  </Card>
-              </div>
-
-              <div class="card" on:mouseenter={() => isHovered = true} on:mouseleave={() => isHovered = false}>
-                <Card img="src\images\TechCastep4.jpg">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">TechCast Ep. 4</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight pb-1">
-                    TechCast, OSC's online guest lecture series, transported students into the minds of industry experts, enriching their knowledge and inspiring innovation.
-                  </p>
-                  <Badge color="dark">
-                    May 25, 2023 | 4 PM
-                  </Badge>
-                </Card>
-              </div>
-
-              <div class="card" on:mouseenter={() => isHovered = true} on:mouseleave={() => isHovered = false}>
-                <Card img="src\images\GDSC.jpg">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">SDG Open Hack</h5>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight pb-6">
-                    SDG Open Hack united minds and technology, fostering collaborative solutions for a sustainable future.
-                  </p>
-                  <Badge color="dark">
-                    Mar 12, 2023 | 10 AM
-                  </Badge>
-                </Card>
-              </div>
-                  
-                  
-                  
-                  
-                    
-            </div>
-            
-          </div>
-	</div>
-</section>
+    </section>
