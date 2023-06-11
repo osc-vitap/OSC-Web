142f2479c7bac8223a5f1479cc3065f945d00eb7
Dhruv Jha <dhruvjha2002@gmail.com>
2023-06-11 12:36:48 +0530

    Completed Nav, major css tweaks, new announcements bar, new testimonial section

--- src/components/ContactSection.svelte
+++ src/components/ContactSection.svelte
@@ -6,13 +6,13 @@
 -->
 
 <section>
-	<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+	<div class="mx-auto max-w-screen-xl px-4 py-20 sm:px-6 lg:px-8">
 		<div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
 			<div class="lg:col-span-2 lg:py-12">
 				<h1 class="text-2xl font-extrabold  text-white sm:text-4xl">
 					Contact Us.
 				</h1>
-				<p class="max-w-xl text-sm mt-0 sm:mt-2 text-white">
+				<p class="max-w-xl text-xs mt-0 sm:mt-2 text-white sm:text-sm">
 					At the same time, the fact that we are wholly owned and totally independent from
 					manufacturer and other group control gives you confidence that we will only recommend what
 					is right for you.
@@ -21,7 +21,7 @@
 				<div class="mt-8">
 					<a href="" class="text-2xl font-bold text-white">+91 0151 475 4450 </a>
 
-					<address class="mt-2 not-italic text-zinc-500">282 Kevin Brook, Imogeneborough, CA 58517</address>
+					<address class="mt-2 not-italic text-white sm:text-zinc-600">282 Kevin Brook, Imogeneborough, CA 58517</address>
 				</div>
 			</div>
 
@@ -75,9 +75,12 @@
 					<div class="mt-4">
 						<button
 							type="submit"
-							class="inline-block w-full text-sm rounded-lg bg-black px-2 py-2 font-medium text-white sm:w-auto"
+							class="inline-block w-full text-sm rounded-lg bg-purple-600 hover:bg-purple-900 px-2 py-2 font-medium text-white sm:w-auto"
 						>
-							Send Enquiry
+<div class="flex items-center justify-center">						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+	<path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
+</svg>
+<span class="ml-2">Send Message</span></div>
 						</button>
 					</div>
 				</form>
--- src/components/TestimonialSection.svelte
+++ src/components/TestimonialSection.svelte
@@ -1,25 +1,31 @@
 <script>
 	import { Card } from 'flowbite-svelte';
-  import TestiMonialCard from './TestiMonialCard.svelte';
+	import TestiMonialCard from './TestiMonialCard.svelte';
 </script>
 
-<div class="w-full max-w-screen-xl py-8 px-6 lg:px-8 ">
-  <div class="flex items-center justify-center flex-col mt-10">
-    <h1 class="text-center animate-text pb-5 bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-3xl font-black">What people have to say 💬</h1>
-    <p class="text-xs max-w-md text-center text-zinc-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque praesentium vero alias aliquam est quas harum reprehenderit quae natus dolor! Eum reiciendis ad autem repellat accusantium temporibus deleniti ipsum sunt?</p>
-  </div>
+<div class="w-full max-w-screen-xl py-8 px-6 lg:px-8">
+	<div class="flex items-center justify-center flex-col mt-10">
+		<h1
+			class="text-center animate-text pb-5 bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-3xl font-black"
+		>
+			What people have to say 💬
+		</h1>
+		<p class="text-xs sm:text-sm max-w-md text-center text-zinc-500 mb-4">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque praesentium vero alias aliquam est
+			quas harum reprehenderit.
+		</p>
+	</div>
 	<div class="flex items-center justify-between my-5">
 		<section>
-			<Card padding="none" size="xl" class="grid md:grid-cols-2 lg:grid-cols-4 rounded-lg">
-        <TestiMonialCard title="Solid foundation for any project" description="Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!"/>
+      <div class="[column-fill:_balance] sm:columns-2 sm:gap-6 lg:columns-3 lg:gap-8">
+        <!-- 6 FOR PROPER ALLIGNMENT -->
         <TestiMonialCard />
         <TestiMonialCard />
         <TestiMonialCard />
         <TestiMonialCard />
         <TestiMonialCard />
         <TestiMonialCard />
-        <TestiMonialCard title="Solid foundation for any project" description="Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!"/>
-			</Card>
+        
 		</section>
 	</div>
 </div>
