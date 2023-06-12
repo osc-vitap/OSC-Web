39598352dd7a47fc54cddcf0134e03026c31855c
Sagnik Sarkar <sagnik.amity@gmail.com>
2023-06-12 21:48:40 +0530

    fixed svg
    Signed-off-by: Sagnik Sarkar <sagnik.amity@gmail.com>

--- src/components/home/LandingInfoCard.svelte
+++ src/components/home/LandingInfoCard.svelte
@@ -1,23 +1,22 @@
 <script>
-	export let title = 'Sample Title';
-	export let description = 'lorem ipsum dolor sit amet consecteur.';
-	export let url = '#';
+  import TestIcon from "../../images/svg/airplane.svg";
+  export let title = "Sample Title";
+  export let description = "lorem ipsum dolor sit amet consecteur.";
+  export let url = "#";
 </script>
 
-<!-- REFER: https://stackoverflow.com/questions/66069647/best-way-to-import-svg-icons-into-a-svelte-app -->
-
-
 <a
-	class="bg-white block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-	href={url}
+  class="bg-white block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+  href={url}
 >
-	<span class="inline-block rounded-lg bg-gray-50 p-3">
-		<!-- SVG WILL GO HERE -->
-	</span>
+  <span class="inline-block rounded-lg bg-gray-50 p-3">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src={TestIcon} class="h-6 w-6" />
+  </span>
 
-	<h2 class="mt-2 font-bold">{title}</h2>
+  <h2 class="mt-2 font-bold">{title}</h2>
 
-	<p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-		{description}
-	</p>
+  <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+    {description}
+  </p>
 </a>
