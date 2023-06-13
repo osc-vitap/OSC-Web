c2f8871838b6b8c6641d28eaa682c93990c76988
ackr-8 <ck@ackr8.com>
2023-06-13 11:40:23 +0530

    Fixed Upcoming Event card in Hero section
    Signed-off-by: ackr-8 <ck@ackr8.com>

--- src/components/home/Hero.svelte
+++ src/components/home/Hero.svelte
@@ -60,16 +60,15 @@
 	
 	<section class="hidden md:block">
 		<div>
-			<Card img="/images/image-1.webp" reverse={vCard}>
+			<Card>
 				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Upcoming Event</h5>
 				<p class="mb-3 font-normal text-gray-500 dark:text-gray-400 leading-tight text-sm">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum natus autem quaerat tempora quos eum delectus perferendis, maiores repellat corporis nisi quibusdam veritatis cumque voluptatum nulla aspernatur necessitatibus vel voluptas!
 				</p>
-				<Button>
+				<Button class="w-fit">
 					Read more <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg>
-				</Button>
+				  </Button>
 			</Card>
-			<Toggle bind:checked={vCard} class="mt-4 italic dark:text-gray-500">Reverse</Toggle>
 		</div>
 	</section>
 </div>
