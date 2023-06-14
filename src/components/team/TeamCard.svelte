dca925fd6b6ddb763b43937102986ed69664da87
Avinash Kumar <avi09sh@gmail.com>
2023-06-14 23:47:01 +0530

    updated teams page with api
    Signed-off-by: Avinash Kumar <avi09sh@gmail.com>

--- src/components/team/TeamCard.svelte
+++ src/components/team/TeamCard.svelte
@@ -1,25 +1,26 @@
 <script>
-  export let image = "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png";
-  export let title = "Sample Name";
-  export let disc = "lorem ipsum dolor sit amet."; 
-  export let link = "#";
+  // export let image = "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png";
+  // export let title = "Sample Name";
+  // export let disc = "lorem ipsum dolor sit amet."; 
+  // export let link = "#";
+  export let member;
 </script>
 
-<a href={link} target="_blank">
+<a href={member.link} target="_blank">
   <section class="mb-5 sm:mb-2 rounded-lg bg-white bg-opacity-90 drop-shadow-md hover:drop-shadow-lg p-2">
     <div class="grid grid-cols-1 gap-1 sm:gap-5 sm:grid-cols-3 sm:items-center sm:min-h-[75px]">
       <img
         alt="OSC"
-        src={image}
+        src={member.image}
         class="aspect-square w-full rounded-lg object-cover drop-shadow-sm shadow-lg shadow-zinc-400/50"
       />
 
       <blockquote class="sm:col-span-2">
         <p class="text-md font-bold sm:text-md text-zinc-800">
-          {title}
+          {member.title}
         </p>
         <p class="text-sm text-zinc-500">
-          {disc}
+          {member.disc}
         </p>
       </blockquote>
     </div>
--- src/components/team/TeamSection.svelte
+++ src/components/team/TeamSection.svelte
@@ -1,18 +1,51 @@
 <script>
   import TeamCard from "./TeamCard.svelte";
+  import { onMount } from "svelte";
+
+  let teamData = {};
+  let years = [];
+  let selectedYear = null;
+
+  async function fetchData() {
+    const response = await fetch("http://localhost:3000/data");
+    teamData = await response.json();
+
+    console.log(teamData);
+
+    years = Object.keys(teamData); // Get the years from the object keys
+  }
+
+  function handleClick(year) {
+    selectedYear = year;
+  }
 </script>
 
 <div class="max-w-screen-xl px-4 sm:px-6 lg:px-8">
+  <div
+    class="[column-fill:_balance] columns-2 md:columns-3 lg:columns-4 gap-5 sm:gap-2 my-2"
+  >
+    <h1>Team Page</h1>
+    <button on:click={fetchData}>Fetch Data</button>
 
-  <div class="[column-fill:_balance] columns-2 md:columns-3 lg:columns-4 gap-5 sm:gap-2 my-2">
+    {#each years as year}
+      <button on:click={() => handleClick(year)}>Year {year}</button>
+    {/each}
+    <div>
+      {#if selectedYear}
+        <h2>Teams for Year {selectedYear}</h2>
+        {#each teamData[selectedYear] as member (member.title)}
+          <TeamCard {member} />
+        {/each}
+      {/if}
+    </div>
+
+    <!-- <TeamCard />
     <TeamCard />
     <TeamCard />
     <TeamCard />
     <TeamCard />
     <TeamCard />
     <TeamCard />
-    <TeamCard />
-    <TeamCard />
+    <TeamCard /> -->
   </div>
-
 </div>
