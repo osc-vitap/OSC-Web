9437ea90f9de23f822ab2fd646f42356e242b0f5
ackr-8 <ck@ackr8.com>
2023-06-16 22:12:38 +0530

    Updated Events card and added events.json
    Signed-off-by: ackr-8 <ck@ackr8.com>

--- src/components/events/EventsSection.svelte
+++ src/components/events/EventsSection.svelte
@@ -3,14 +3,12 @@
 </script>
 
 <div class="max-w-screen-xl px-4 sm:px-6 lg:px-8">
-	<div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 max-w-full">
-		<h1>Sample</h1>
-		<h1>Sample</h1>
-		<h1>Sample</h1>
-		<h1>Sample</h1>
-		<h1>Sample</h1>
-		<h1>Sample</h1>
-		<h1>Sample</h1>
-		<h1>Sample</h1>
+	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-full gap-2">
+		<EventCard />
+		<EventCard />
+		<EventCard />
+		<EventCard />
+		<EventCard />
+		<EventCard />
 	</div>
 </div>
